ad061e6518067772b0c1e0b211cb25907b4f56d2
Konhardt, Andre <ankonhardt@gmx.de>
2019-09-18 13:25:28 +0200

    angular bindings

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Comp1Component implements OnInit {
 
-  constructor() { }
+  serverId: number = 10;
+  serverStatus: string = "offline";
+
+  allowServer: boolean = false;
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverStatus = "online";
+    console.log("Create Server");
+  }
+
+  onUpdateStatus(val) {
+    this.serverStatus = val;
+  }
 
   ngOnInit() {
   }
