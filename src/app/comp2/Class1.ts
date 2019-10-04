388028423ec16c932163032cbf355ea61fa40120
Konhardt, Andre <andre.konhardt@stud.hs-ruhrwest.de>
2019-10-04 22:06:15 +0200

    qwe

---
+++ src/app/comp2/Class1.ts
@@ -0,0 +1,21 @@
+export class Class1 {
+    x : number = 5;
+    y : number = 1;
+    name : string = "myName";
+
+    static name2 : string = "myName2";
+
+    names : string[] = ["Name1", "Name2"];
+}
+
+ class Class2 {
+    x : number = 4;
+    y : number = 3;
+    el : number = 22;
+    name : string = "myName";
+
+    static name2 : string = "myName2";
+
+    names : string[] = ["Name1", "Name2"];
+}
+export { Class2 };
--- src/app/comp2/comp2.component.ts
+++ src/app/comp2/comp2.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import with Alias
+import { Class1 as El} from './Class1';
+import { Class2 } from './Class1';
+
+// import that requires default export
+
+// import Class1 from './Class1';
+// import Class2 from './Class1';
+
 @Component({
   selector: 'app-comp2',
   templateUrl: './comp2.component.html',
@@ -7,7 +16,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Comp2Component implements OnInit {
 
-  constructor() { }
+  // Create Class Instance
+  a : number = 5;
+  c1 : El = new El();
+  c2 : Class2 = new Class2();
+  ar : number[] = [11, 22, 33];
+
+  constructor() {
+    this.c1.name = "qwe";
+    console.log(this.c1.name);
+
+    // For in / of Loops
+    for (let i of this.ar) {
+      console.log("Of Loop : " + i);   // 11, 22, 33
+    }
+
+    for (let i in this.ar) {
+      console.log("In Loop : " + i);   // 0, 1, 2
+    }
+
+  }
 
   ngOnInit() {
   }
